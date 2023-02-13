a32be42cde9582cd3319dff46b2f8282f96deae1
Sawmi-6126 <sawmi.sutha99@gmail.com>
2023-02-13 15:34:07 +0000

    layout

--- src/layout/RepairNavLayout.js
+++ src/layout/RepairNavLayout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import RepairNav from "./Components/RepairNav.js";
+import RepairNav from "../Components/RepairNav.js";
 
 function RepairNavLayout() {
   return (
